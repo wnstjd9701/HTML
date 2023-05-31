cb9780debc14869a7c5e6233dbcc9b13fa64cda3
wnstjd9701 <wnstjd9701@naver.com>
2023-05-31 18:01:31 +0900

    add

--- HTML_KOSA/Project/cart/cart.js
+++ HTML_KOSA/Project/cart/cart.js
@@ -1,9 +1,13 @@
+window.onload = () => {
+
+}
 const addAmount = (event) => {
     // const productsList = document.getElementsByClassName("table_row");
     // console.log(productsList.length); // 다음에 추가할 때 product_(length+1)
     const btnId = event.target.id.split('_');
     const productId = btnId[2];
-    const amount = document.getElementById("product_" + "amount_" + productId);
+    
+    const amount = document.getElementById(`product_amount_${productId}`);
 
     const checkBox = document.querySelectorAll("input[name=product]");
     // 체크가 안되어 있는데 상품 수량을 변경할 경우 체크 해줌 / 상품 인덱스는 1부터 시작하므로 checkBox의 인덱스는 productId - 1
@@ -19,7 +23,8 @@ const addAmount = (event) => {
 const minusAmount = (event) => {
     const btnId = event.target.id.split('_');
     const productId = btnId[2];
-    const amount = document.getElementById("product_" + "amount_" + productId);
+    
+    const amount = document.getElementById(`product_amount_${productId}`);
 
     const checkBox = document.querySelectorAll("input[name=product]");
     // 체크가 안되어 있는데 상품 수량을 변경할 경우 체크 해줌 / 상품 인덱스는 1부터 시작하므로 checkBox의 인덱스는 productId - 1
@@ -55,23 +60,24 @@ const totalPriceCalculate = () => {
         productPrice = productPrice.replace(/,/g, '');
 
         // 상품 수량 가져오기
-        let productAmount = document.getElementById("product_" + "amount_" + productId);
+        let productAmount = document.getElementById(`product_amount_${productId}`);
 
         // 총 금액 = 상품 수량 * 상품 가격
         selectProductPrice += parseInt(productPrice) * parseInt(productAmount.innerHTML);
-        console.log(selectProductPrice);
     })
 
     // 배송비 
     let deliveryFee = document.getElementById("delivery_fee");
-    if (selectProductPrice > 500000) {
-        deliveryFee.innerHTML = "배송비 0";
+    if (selectProductPrice > 500000 || selectProductPrice == 0) {
+        deliveryFee.innerHTML = "0<span>&nbsp;\\</span>";
     } else {
-        deliveryFee.innerHTML = "배송비 3,000";
+        deliveryFee.innerHTML = "3,000<span>&nbsp;\\</span>";
     }
 
-    let price = document.getElementById("total_price");
-    price.innerHTML = selectProductPrice.toLocaleString();
+    let subTotalPrice = document.getElementById("sub_total_price");
+    let totalPrice = document.getElementById("total_price");
+    subTotalPrice.innerHTML = selectProductPrice.toLocaleString() + "<span>&nbsp;\\</span>";
+    totalPrice.innerHTML = (parseInt(deliveryFee.innerHTML.split(',').join('')) + selectProductPrice).toLocaleString() + "<span>&nbsp;\\</span>";
 }
 
 const checkSelectAll = () => {
@@ -92,6 +98,7 @@ const checkSelectAll = () => {
         // 전체 선택 체크박스 해제
         selectAll.checked = false;
     }
+    totalPriceCalculate();
 }
 
 const selectAll = (selectBox) => {
@@ -100,4 +107,26 @@ const selectAll = (selectBox) => {
     checkBoxes.forEach((checkBox) => {
         checkBox.checked = selectBox.checked;
     })
+    totalPriceCalculate();
 }
+
+const deleteSelectProduct = () => {
+    // 체크된 productList 가져오기
+    const checkedProductsList = document.querySelectorAll('input[name=product]:checked');
+    const productRowList = document.querySelectorAll(".table_row");
+    const productIdList = [];
+    // productId 가져오기
+    checkedProductsList.forEach((productInfo) => {
+        productIdList.push(productInfo.id.split('_')[2]);
+    })
+    productIdList.forEach((productId) => {
+        productRowList[productId-1].innerHTML = "";
+    })
+    totalPriceCalculate();
+}
+
+const changeProductPrice = () => {
+    const orderPrices = document.querySelectorAll(".order_price");
+    console.log(orderPrices[0]);
+}
+
