c2c7b0ba052f52c44fcc27123bd5baa325171aee
wnstjd9701 <wnstjd9701@naver.com>
2023-06-05 18:24:17 +0900

    HTML

--- HTML_KOSA/Project/payments/payments.js
+++ HTML_KOSA/Project/payments/payments.js
@@ -1,3 +1,22 @@
+window.onload = () => {
+  // const test = new TypeIt(".top_name", {
+  //   strings: "결제 페이지",
+  //   speed: 200,
+  //   waitUntilVisible: true,
+  // }).go();
+  // const productNameList = document.querySelectorAll('.product_name');
+  // console.log(productNameList);
+
+  // productNameList.forEach((productName) => {
+  //   console.log(productName.id);
+  //   new TypeIt(`#${productName.id}`, {
+  //     strings: `${productName.id}`,
+  //     speed: 200,
+  //     waitUntilVisible: true,
+  //   }).go();
+  // })
+};
+
 const autoHyphen = (target) => {
   target.value = target.value
     .replace(/[^0-9]/g, '')
